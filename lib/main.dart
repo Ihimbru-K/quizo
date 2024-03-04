b769c17556c64e541404bd78c39f11b0c2e3bb0b
Ihimbru-k <kanihims25@gmail.com>
2024-03-04 11:04:24 +0100

    added some explanations on material widgets

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,8 @@ runApp(MyApp());
 
 class MyApp extends StatelessWidget {
 
+  // build method is needed because flutter must always call it when it wants to draw something to the screen
+  //It is an aspect of all material widgets
   build(BuildContext context) {
     return MaterialApp(
       home: Text("Hello Kboy"),

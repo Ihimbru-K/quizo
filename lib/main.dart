ecd5eb1032a70d23ec6bfdba7670291f0866cc72
Ihimbru-k <kanihims25@gmail.com>
2024-03-04 11:07:09 +0100

    added explanations on buildcontexts

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   // build method is needed because flutter must always call it when it wants to draw something to the screen
   //It is an aspect of all material widgets
   build(BuildContext context) {
+    //buildcontext holds data about the widget and its position in the widget tree. It is managed by the flutter sdk
     return MaterialApp(
       home: Text("Hello Kboy"),
     );

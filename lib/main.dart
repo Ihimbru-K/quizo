8483ebaf243464c78d669da08f95e5bf7145e233
Ihimbru-k <kanihims25@gmail.com>
2024-03-04 11:24:02 +0100

    added a scaffold widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,25 @@
 import 'package:flutter/material.dart';
 
 //Every widget in flutter is a dart class that has a build method
-void main(){
 
-runApp(MyApp()); //runapp method calls the build method of my app widget
-
-}
+void main() =>
+    runApp(MyApp()); // arrow functions for functions that don't do much
 
 class MyApp extends StatelessWidget {
-
   // build method is needed because flutter must always call it when it wants to draw something to the screen
   //It is an aspect of all material widgets
   @override // specifying that you added a method in an already inbuilt class
   build(BuildContext context) {
     //buildcontext holds data about the widget and its position in the widget tree. It is managed by the flutter sdk
     return MaterialApp(
-      home: Text("Hello Kboy"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My Quiz app"),
+        ),
+        body: Center(
+          child: Text("Here is the body"),
+        ),
+      ),
     );
   }
 }
-
-
-
-
-

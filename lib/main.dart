9b6d791e64ce3ed78701b641491c25054aca4693
Ihimbru-k <kanihims25@gmail.com>
2024-03-06 12:59:55 +0100

    refactored stateless to statefull widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,53 @@
 import 'package:flutter/material.dart';
 
-//Every widget in flutter is a dart class that has a build method
+void main() => runApp(MyApp());
 
-void main() =>
-    runApp(MyApp()); // arrow functions for functions that don't do much
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
 
-class MyApp extends StatelessWidget {
-  void answerQuestion() => print('Answer question');
-  // build method is needed because flutter must always call it when it wants to draw something to the screen
-  //It is an aspect of all material widgets
-  @override // specifying that you added a method in an already inbuilt class
-  build(BuildContext context) {
-    //buildcontext holds data about the widget and its position in the widget tree. It is managed by the flutter sdk
+class _MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {  //updates only this widget only when the build method is called
+      questionIndex += 1;
+    });
+    print(questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      "What's your name",
+      "What's your favorite colour?",
+      "Which school are you from"
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("My Quiz app"),
         ),
         body: Center(
-          child: Column(children: [
-            ElevatedButton(onPressed: null, child: Text('Answer one')),
-            ElevatedButton(onPressed: null, child: Text('Answer two')),
-
-            //when passing a function to onPress, we are passing the name of the function and not the function itself
-            ElevatedButton(onPressed: answerQuestion, child: Text('Answer three')),
-//push this code
-
-          ],)
+          child: Column(
+            children: [
+              ElevatedButton(
+                onPressed: () => answerQuestion(),
+                child: Text(questions[questionIndex]),
+              ),
+              ElevatedButton(
+                onPressed: () => print("Answer this question"),
+                child: Text(questions[1]),
+              ),
+              ElevatedButton(
+                onPressed: () => answerQuestion(),
+                child: Text('Answer three'),
+              ),
+            ],
+          ),
         ),
       ),
     );

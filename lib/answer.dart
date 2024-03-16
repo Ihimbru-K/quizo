06ac57cb7062ac9752da0692ef70de15db66ce49
Ihimbru-k <kanihims25@gmail.com>
2024-03-16 01:50:33 +0100

    fixed a bug

--- lib/answer.dart
+++ lib/answer.dart
@@ -1,30 +1,27 @@
 import 'package:flutter/material.dart';
 
-class Answer extends StatelessWidget{
+class Answer extends StatelessWidget {
 //VoidCallback can be used but the only diff is that it precises a function with void parameters and which returns nothing too
-final Function selectHandler;
+  final Function selectHandler;
 
   Answer(this.selectHandler);
- // const Answer({Key? key}) : super(key: key);
+
+  // const Answer({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
+    return Container(
+      child: ElevatedButton(
+        onPressed: () {
+          selectHandler();
+        },
+        child: Text('answer question'),
+        style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue //Colors is a class
+            //blue is a static property because it doesn't need the class to be instanced before it is called
 
-      child: ElevatedButton(onPressed: () {
-      }, child: Text('answer question'),
-
-
-
-style: ElevatedButton.styleFrom(backgroundColor: Colors.blue  //Colors is a class
-  //blue is a static property because it doesn't need the class to be instanced before it is called
-
+            ),
       ),
-
-
-      ),
-
-
     );
   }
 }

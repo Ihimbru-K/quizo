6567b1b89c5ee3a982463d0db08ea2bb065b63ed
Ihimbru-k <kanihims25@gmail.com>
2024-03-16 01:01:34 +0100

    working on the answer question custom widget

--- lib/answer.dart
+++ lib/answer.dart
@@ -5,6 +5,22 @@ class Answer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Container();
+    return  Container(
+
+      child: ElevatedButton(onPressed: () {
+      }, child: Text('answer question'),
+
+
+
+style: ElevatedButton.styleFrom(backgroundColor: Colors.blue  //Colors is a class
+  //blue is a static property because it doesn't need the class to be instanciated before it is called
+
+      ),
+
+
+      ),
+
+
+    );
   }
 }
